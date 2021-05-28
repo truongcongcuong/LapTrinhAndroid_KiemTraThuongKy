de0e9cde0dbed3566cc951089dbcd124ea18534b
Cuong <truongcongcuong0302@gmail.com>
2021-05-28 12:49:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/laptrinhandroid_kiemtrathuongky/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.laptrinhandroid_kiemtrathuongky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
